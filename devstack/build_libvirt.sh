17da6743581f081d67b14d23d69cd02f2e53fe0e
Michele Paolino <m.paolino@virtualopensystems.com>
2016-05-06 16:21:11 +0200

    Plugin to setup libvirt/QEMU from tar releases
    This patch enables the devstack installation of custom qemu/libvirt
    versions from tar releases.
    
    How to enable the plugin
    ========================
    
    1 Download devstack::
    
         git clone https://git.openstack.org/openstack-dev/devstack
    
    2. Add devstack-plugin-tar-installer external repository in the local.conf file::
    
         > cat local.conf
         [[local|localrc]]
         enable_plugin devstack-tar-installer-plugin git@git.virtualopensystems.com:sesame/devstack-qemu-libvirt-from-tar-plugin.git
    
    3. Configure the plugin by definig these variables in the local.conf file::
         QEMU_VERSION=2.1.5
         LIBVIRT_VERSION=1.2.9
    
    3. run ``stack.sh``
    
    Tested with Ubuntu Trusty, QEMU 2.6.0-rc1 and libvirt 1.2.7
    
    Change-Id: I0bfffe52be66b9da8f878dc8f807faf187cf3217
    Signed-off-by: Michele Paolino <m.paolino@virtualopensystems.com>

---
+++ devstack/build_libvirt.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2015-2016 Virtual Open Systems, SAS.
+#
+# Michele Paolino <m.paolino@virtualopensystems.com>
+# Virtual Open Systems devstack-plugin-tar-installer
+#
+# build_libvirt.sh - Install libvirt and its dependencies.
+#
+
+
+# Echo commands
+set -o xtrace
+
+# Exit on error to stop unexpected errors
+set -o errexit
+
+function usage {
+    echo "$0 - Install libvirt from tar releases."
+    echo ""
+    echo "Usage: $0 <LIBVIRT_VERSION>"
+    echo ""
+    echo "Example: $0 1.3.3"
+}
+
+# Keep track of the current directory
+TOOLS_DIR=${TOOLS_DIR:-$(cd $(dirname "$0") && pwd)}
+TOP_DIR=${TOP_DIR:-$(cd $TOOLS_DIR/..; pwd)}
+
+# Import common functions and variables
+source $TOOLS_DIR/functions
+source $TOOLS_DIR/stackrc
+
+# Find the cache dir
+SOURCE_FILE=$TOP_DIR/files
+
+# Reclone/configure libvirt each time the script is run
+RECLONE=$(trueorfalse False $RECLONE)
+
+# If this value is not user defined, use the official link
+LIBVIRT_URL_BASE=${LIBVIRT_URL_BASE:-https://libvirt.org/sources/}
+
+# Getting program version from the bash argument
+LIBVIRT_VERSION=$1
+# libvirt is released as .tar.gz
+LIBVIRT_FILE=libvirt-"$LIBVIRT_VERSION".tar.gz
+LIBVIRT_DIR="$DEST"/libvirt-"$LIBVIRT_VERSION"
+LIBVIRT_URL="$LIBVIRT_URL_BASE""$LIBVIRT_FILE"
+
+# libvirtd debug variables
+NOW=`date +"%F-%T"`
+WHERE=`hostname`
+EPOCH=`date +%s`
+
+
+if [[ -z "$1" ]]; then
+    usage
+    exit 1
+fi
+
+if [[ "$1" == remove ]]; then
+    echo "Removing libvirt $LIBVIRT_VERSION from the system"
+    rm "$SOURCE_FILE"/"$LIBVIRT_FILE"
+    cd "$LIBVIRT_DIR"
+    sudo make uninstall
+    cd ..
+    rm -r "$LIBVIRT_DIR"
+else
+    echo "Installing libvirt $LIBVIRT_VERSION"
+    echo "Installing libvirt build dependencies"
+    if is_ubuntu; then
+        sudo apt-get build-dep libvirt -y
+        install_package python-guestfs
+            if [[ ${DISTRO} =~ (precise) ]]; then
+                sudo apt-get install libnl-route-3-dev -y
+            fi
+    elif is_fedora || is_suse; then
+        install_package yum-utils
+        sudo dnf builddep libvirt -y
+        install_package python-libguestfs
+    fi
+
+    echo "Downloading the libvirt sources"
+    wget -N "$LIBVIRT_URL" -P "$SOURCE_FILE"
+
+    if [[ ! -d "$LIBVIRT_DIR" || "$RECLONE" = "True" ]]; then
+        echo "Configuring libvirt"
+        tar -xf "$SOURCE_FILE"/"$LIBVIRT_FILE" -C "$DEST"
+        cd "$LIBVIRT_DIR"
+        ./configure --prefix=/ --exec-prefix=/usr \
+            --with-packager="OpenStack DevStack $WHERE $NOW" \
+            --with-packager_version="$EPOCH"
+    fi
+
+    # Polkit configurations
+    if is_ubuntu; then
+        cat <<EOF | sudo tee /etc/polkit-1/localauthority/50-local.d/50-libvirt-remote-access.pkla
+[libvirt Management Access]
+Identity=unix-user:$STACK_USER
+Action=org.libvirt.unix.manage
+ResultAny=yes
+ResultInactive=yes
+ResultActive=yes
+EOF
+    fi
+
+    echo "Compiling libvirt"
+    cd "$LIBVIRT_DIR"
+    make -j"$(nproc)"
+    sudo make install
+fi
+
+cd "$TOOLS_DIR"
